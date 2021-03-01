7eab5bb4ba85308b882b89688ee3fda5265e8cab
johnshields <g00348436@gmit.ie>
2021-03-01 19:25:34 +0000

    ROTL & ROTR functions.

--- program/main.c
+++ program/main.c
@@ -3,8 +3,8 @@
  * SHA-512 Calculator
  * A program in the C programming language to calculate the SHA512 (Secure Hash Standard) value of an input file.
  *
+ * [1] https://www.nist.gov/publications/secure-hash-standard
  * https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
- * https://www.nist.gov/publications/secure-hash-standard
  * https://www.geeksforgeeks.org/bitwise-operators-in-c-cpp/
  * https://www.guru99.com/c-bitwise-operators.html
  */
@@ -12,35 +12,39 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#define W 64
 #define WORD uint64_t
 #define PF PRIX64
 
-// TODO - ROTR & SHR - little SIGMA - big SIGMA
+// Page 5 of the secure hash standard. [1]
+#define ROTL(x, n) (x<<n) | (x>>(W-n))
+#define ROTR(x, n) (x>>n) | (x<<(W-n))
 
-// Ch(x, y, z) = (x  y)(x  z)
-WORD Ch(WORD x, WORD y, WORD z) {
-    return (x & y) ^ (~x & z);
-}
-
-// Maj(x, y, z) = (x  y)(x  z)(y  z)
-WORD Maj(WORD x, WORD y, WORD z) {
-    return (x & y) ^ (~x & z) ^ (y & z);
-}
+// Page 10 of the secure hash standard. [1]
+#define CH(x, y, z) (x & y) ^ (~x & z)
+#define MAJ(x, y, z) (x & y) ^ (x & z) ^ (y & z)
 
 
 int main(int argc, char *argv[]) {
     printf("SHA-512 Calculator\n");
+
     // x picks out the 1s and 0s from the corresponding position
     // where X has 0s picks out the 1s and 0s in z.
     // Use x to choose bits from y & z and merge them together.
-    WORD x = 0x0F0F0F0F;
+    WORD x = 0xF1234567;
     WORD y = 0x0A0A0A0A;
     WORD z = 0xB0B0B0B0;
 
-    WORD ans = Ch(x, y, z);
-    WORD res = Maj(x, y, z);
+    WORD ans;
+
+    ans = CH(x, y, z);
     printf("Ch(%08"PF",%08"PF",%08"PF")=%08"PF"\n", x, y, z, ans);
-    printf("Maj(%08"PF",%08"PF",%08"PF")=%08"PF"\n", x, y, z, res);
+
+    ans = MAJ(x, y, z);
+    printf("Maj(%08"PF",%08"PF",%08"PF")=%08"PF"\n", x, y, z, ans);
+
+    printf("ROTL(%08" PF " -> %08" PF "\n", x, ROTL(x, 4));
+    printf("ROTR(%08" PF " -> %08" PF "\n", x, ROTR(x, 4));
 
     return 0;
 }
